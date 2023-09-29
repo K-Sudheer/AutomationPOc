3d33683562aa4e809f52caeffcd157dbd9f071e5
Kokolu Sudheer <sudheer.kokolu@sonata-software.com>
2023-09-29 13:32:39 +0530

    Commit Full Automation Code

--- Bluestem/automationDemo/automationDemo/src/main/java/com/example/service/DBQuery.java
+++
@@ -1,6 +0,0 @@
-package com.example.service;
-
-public class DBQuery {
-
-    public static String TABLE1_QRY="SELECT id, sal, email_id, name FROM employees WHERE id = ? and upper(name)=? and sal = ? and upper(email_id) = ?";
-}
